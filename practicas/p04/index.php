efc76ba212773865f49ead6ee99c425e4edf3fd9
Kippie01PW <acarroflores65@gmail.com>
2025-02-04 16:00:39 -0600

    Se añadieron los incisos 1 y 2 de la práctica

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -2,24 +2,19 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Práctica 3</title>
+    <title>Práctica 4</title>
 </head>
 <body>
     <h2>Ejercicio 1</h2>
     <p>Determina cuál de las siguientes variables son válidas y explica por qué:</p>
     <p>$_myvar,  $_7var,  myvar,  $myvar,  $var7,  $_element1, $house*5</p>
     <?php
-        //AQUI VA MI CÓDIGO PHP
         $_myvar;
         $_7var;
-        //myvar;       // Inválida
         $myvar;
         $var7;
         $_element1;
-        //$house*5;     // Invalida
-        
         echo '<h4>Respuesta:</h4>';   
-    
         echo '<ul>';
         echo '<li>$_myvar es válida porque inicia con guión bajo.</li>';
         echo '<li>$_7var es válida porque inicia con guión bajo.</li>';
@@ -29,6 +24,35 @@
         echo '<li>$_element1 es válida porque inicia con guión bajo.</li>';
         echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
         echo '</ul>';
+
+        unset($_myvar);
+        unset($_7var);
+        unset($_element1);
+        unset($myvar);
+        unset($var7);
     ?>
+    
+    <h2>Ejercicio 2</h2>
+    <p>Proporcionar los valores de $a, $b, $c</p>
+    <?php
+    $a = "Manejador SQL"; 
+    $b = 'MySQL';
+    $c = &$a;
+
+    echo $a . '<br>';
+    echo $b . '<br>';
+    echo $c . '<br>';
+    echo '<br>'; 
+
+    $a = "PHP Server"; 
+    $b = &$a; 
+
+    echo $a . "<br>";
+    echo $b . "<br>";
+    unset($a);
+    unset($b);
+    unset($c);
+    ?>
+
 </body>
 </html>
