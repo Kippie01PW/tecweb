261d8761c1dd3d38dbfb9bfe20a93e2c1fb9d621
Kippie01PW <acarroflores65@gmail.com>
2025-02-27 20:23:19 -0600

    Se realizó correctamente la colocación de datos de forma dinámica en el formulario correspondiente al inciso 3

--- practicas/p09/formulario_productos_v2.php
+++ practicas/p09/formulario_productos_v2.php
@@ -25,10 +25,10 @@
         <legend>Ingresa la información del Producto</legend>
 
         <ul>
-          <li><label for="form-name">Nombre del producto:</label> <input type="text" name="name" id="form-name" onfocus="limpiarMensajesRepuesta1()" onblur="verificarNombre()"><div id="respuesta1"></div></li>
+          <li><label for="form-name">Nombre del producto:</label> <input type="text" name="name" id="form-name" value="<?= !empty($_POST['name'])?$_POST['name']:$_GET['name'] ?>" onfocus="limpiarMensajesRepuesta1()" onblur="verificarNombre()"><div id="respuesta1"></div></li>
 
             <li><p>Selecciona una marca:</p></li>
-              <select name="brand" id="form-brand" size="1" onblur="seleccionarMarca()" onfocus="limpiarMensajeRespuesta2()">
+              <select name="brand" id="form-brand" value="<?= !empty($_POST['brand'])?$_POST['brand']:$_GET['brand'] ?>" size="1" onblur="seleccionarMarca()" onfocus="limpiarMensajeRespuesta2()">
                 <option value="">Selecciona una opción</option>
                 <option value="Apple">Apple</option>
                 <option value="Samsung">Samsung</option>
@@ -42,11 +42,13 @@
               </select>
               <div id="respuesta2"></div>
   
-          <li><label for="form-model">Modelo del producto:</label> <input type="text" name="model" id="form-model" onfocus="limpiarMensajesRepuesta3()" onblur="verificarModelo()"><div id="respuesta3"></div></li>
-          <li><label for="form-price">Precio del producto:</label> <input type="text" name="price" id="form-price" onfocus="limpiarMensajesRepuesta4()" onblur="verificarPrecio()"><div id="respuesta4"></div></li>
-          <li><label for="form-description">Escribe la descripción del producto</label><br><textarea name="description" rows="4" cols="60" id="form-description" placeholder="Escribe algo aquí..." onfocus="limpiarMensajesRepuesta5()" onblur="controlarCaracteres()"></textarea><div id="respuesta5"></div></li>
-          <li><label for="form-unit">Unidades del producto:</label> <input type="text" name="unit" id="form-unit" onfocus="limpiarMensajesRepuesta6()" onblur="verificarUnidades()"><div id="respuesta6"></div></li>
-          <li><label for="form-imagen">Opcionalmente pon una ruta de imagen:</label> <input type="text" name="imagen" id="form-imagen"></li>
+          <li><label for="form-model">Modelo del producto:</label> <input type="text" name="model" id="form-model" value="<?= !empty($_POST['model'])?$_POST['model']:$_GET['model'] ?>" onfocus="limpiarMensajesRepuesta3()" onblur="verificarModelo()"><div id="respuesta3"></div></li>
+          <li><label for="form-price">Precio del producto:</label> <input type="text" name="price" id="form-price" value="<?= !empty($_POST['price'])?$_POST['price']:$_GET['price'] ?>" onfocus="limpiarMensajesRepuesta4()" onblur="verificarPrecio()"><div id="respuesta4"></div></li>
+          <li><label for="form-description">Escribe la descripción del producto</label><br><textarea name="description" rows="4" cols="60" id="form-description" placeholder="Escribe algo aquí..." onfocus="limpiarMensajesRepuesta5()" onblur="controlarCaracteres()">
+          <?= !empty($_POST['description']) ? htmlspecialchars($_POST['description']) : (isset($_GET['description']) ? htmlspecialchars($_GET['description']) : '') ?>
+          </textarea><div id="respuesta5"></div></li>
+          <li><label for="form-unit">Unidades del producto:</label> <input type="text" name="unit" id="form-unit" value="<?= !empty($_POST['unit'])?$_POST['unit']:$_GET['unit'] ?>" onfocus="limpiarMensajesRepuesta6()" onblur="verificarUnidades()"><div id="respuesta6"></div></li>
+          <li><label for="form-imagen">Opcionalmente pon una ruta de imagen:</label> <input type="text" id="form-imagen" name="imagen" value="<?= !empty($_POST['imagen'])?$_POST['imagen']:$_GET['imagen'] ?>"> </li>
         </ul>
       </fieldset>
 
--- practicas/p09/get_productos_vigentes_v2.php
+++ practicas/p09/get_productos_vigentes_v2.php
@@ -11,14 +11,34 @@
             height: 100px;
         }
     </style>
+
+        <script>
+        function show(event) {
+            var rowId = event.target.parentNode.parentNode.id;
+            var data = document.getElementById(rowId).querySelectorAll(".row-data");
+
+            var id = rowId;
+
+            var name = data[0].innerHTML;
+            var brand = data[1].innerHTML;
+            var model = data[2].innerHTML;
+            var price = data[3].innerHTML;
+            var description = data[5].innerHTML;
+            var unit = data[4].innerHTML;
+            var imagen = data[6].querySelector('img').src;
+
+            alert("ID: " + id + "\nNombre: " + name + "\nMarca: " + brand + "\nModelo: " + model + "\nPrecio: " + price + "\nUnidades: " + unit + "\nDetalles: " + description + "\nRuta de imagen: " + imagen);
+
+            send2form(name, brand, model, price, description, unit, imagen);
+        }
+        </script>
+
 </head>
 <body>
     <h3>PRODUCTOS</h3>
     <p></p>
 
     <?php
-
-
         /** SE CREA EL OBJETO DE CONEXIÓN */
         @$link = new mysqli('localhost', 'root', 'andrei2703', 'marketzone');
 
@@ -41,23 +61,26 @@
                         <th scope="col">Detalles</th>
                         <th scope="col">Imagen</th>
                         <th scope="col">Eliminado</th>
+                        <th scope="col">Modificar</th>
                         </tr>
                     </thead>
                     <tbody>';
 
             while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
-                echo '<tr>
+                echo '<tr id="' . $row['id'] . '">
                         <th scope="row">' . $row['id'] . '</th>
-                        <td>' . $row['nombre'] . '</td>
-                        <td>' . $row['marca'] . '</td>
-                        <td>' . $row['modelo'] . '</td>
-                        <td>' . $row['precio'] . '</td>
-                        <td>' . $row['unidades'] . '</td>
-                        <td>' . $row['detalles'] . '</td>
-                        <td><img src="' . $row['imagen'] . '" alt="Imagen del producto" /></td>
+                        <td class="row-data">' . $row['nombre'] . '</td>
+                        <td class="row-data">' . $row['marca'] . '</td>
+                        <td class="row-data">' . $row['modelo'] . '</td>
+                        <td class="row-data">' . $row['precio'] . '</td>
+                        <td class="row-data">' . $row['unidades'] . '</td>
+                        <td class="row-data">' . $row['detalles'] . '</td>
+                        <td class="row-data"><img src="' . $row['imagen'] . '" alt="Imagen del producto" /></td>
                         <td>' . $row['eliminado'] . '</td>
-                    </tr>';
+                        <td><input type="button" value="modificar" onclick="show(event)" /></td>
+                        </tr>';
             }
+                    
 
             echo '</tbody></table>';
 
@@ -70,7 +93,56 @@
         $link->close();
     
     ?>
-    
+        <script>
+        function send2form(name, brand, model, price, description, unit, imagen) {
+            var form = document.createElement("form");
+
+            var nameIn = document.createElement("input");
+            nameIn.type = 'text';
+            nameIn.name = 'name';
+            nameIn.value = name;
+            form.appendChild(nameIn);
+
+            var modelIn = document.createElement("input");
+            modelIn.type = 'text';
+            modelIn.name = 'model';
+            modelIn.value = model;
+            form.appendChild(modelIn);
+
+            var priceIn = document.createElement("input");
+            priceIn.type = 'text';
+            priceIn.name = 'price';
+            priceIn.value = price;
+            form.appendChild(priceIn);
+
+            var descriptionTextarea = document.createElement("textarea");
+            descriptionTextarea.name = 'description';
+            descriptionTextarea.rows = 4; 
+            descriptionTextarea.cols = 60; 
+            descriptionTextarea.value = description; 
+            form.appendChild(descriptionTextarea);
+
+            var unitIn = document.createElement("input");
+            unitIn.type = 'text';
+            unitIn.name = 'unit';
+            unitIn.value = unit;
+            form.appendChild(unitIn);
+
+            var imagenIn = document.createElement("input");
+            imagenIn.type = 'text';
+            imagenIn.name = 'imagen';
+            imagenIn.value = imagen;
+            form.appendChild(imagenIn);
+            console.log(form);
+
+            form.method = 'POST';
+            form.action = 'formulario_productos_v2.php';  
+
+            document.body.appendChild(form);
+            form.submit();
+        }
+    </script>
+
     <p>
     <a href="https://validator.w3.org/markup/check?uri=referer"><img
       src="https://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" /></a>
