4b249b143f01a9ad98299adfbc16b6386479ef79
Kippie01PW <acarroflores65@gmail.com>
2025-03-12 17:34:25 -0600

    Se ha agregado la actividad de POO en php

---
+++ actividades/POO/05-colaboracion/Pagina.php
@@ -0,0 +1,42 @@
+<?php
+
+class Pagina{
+    private $cabecera;
+    private $cuerpo; 
+    private $pie;
+
+    public function __construct($texto1, $texto2){
+        $this->cabecera = new Cabecera ($texto1);
+        $this->cuerpo = new Cuerpo; 
+        $this->pie = new Pie($texto2);
+    }
+
+    public function insertar_cuerpo($texto){
+        $this->cuerpo->insertar_parrafo($texto); 
+    }
+
+    public function graficar (){
+        $this->cabecera->graficar();
+        $this->cuerpo->graficar();
+        $this->pie->graficar();
+    }
+}
+
+/**
+ * Implementar las clases de Cabecera, Cuerpo y Pie
+ * 
+ * 1. La clase Cabecera tiene las siguientes características
+ *  >Tiene un constructor que recibe un texto e inicializa un atributo de nombre titulo.
+ *  >Tiene una función graficar, que utiliza un encabezado de nivel 1, a partir de un texto y un estilo.
+ * 
+ * 2.La clase Cuerpo tiene las siguientes características:
+ *  > No tiene constructor pero tiene un atributo privado que corresponde a un arreglo de lineas de texto, el atributo
+ * se debe llamar lineas.
+ *  > Tiene una función graficar que recorre el atributo lineas para mostrar elementos <p> que contiene el texto
+ * dentro del arreglo.
+ * 
+ * 3. La clase Pie tiene las siguientes características
+ *  >Tiene un constructor que recibe un texto e inicializa un atributo de nombre mensaje.
+ *  >Tiene una función graficar, que utiliza un encabezado de nivel 4, a partir de un texto y un estilo por defecto.
+ */
+?>
